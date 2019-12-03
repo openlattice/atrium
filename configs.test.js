f3e6b7af13ae1ba4d37e68939f5b9407c703c46e
Hristo Oskov <hristo@openlattice.com>
2019-12-03 14:14:58 -0800

    atrium is now a git superproject

--- configs.test.js
+++ configs.test.js
@@ -2,10 +2,11 @@ const configs = require('./configs');
 
 describe('webpack config paths', () => {
 
-  test('apps should be in the same directory as atrium', () => {
+  test('atrium apps should be submodules', () => {
     expect(configs).toEqual([
-      '../lattice-login/config/webpack/webpack.config.dev.js',
-      '../lattice-edm/config/webpack/webpack.config.dev.js',
+      './lattice-edm/config/webpack/webpack.config.dev.js',
+      './lattice-login/config/webpack/webpack.config.dev.js',
+      './lattice-orgs/config/webpack/webpack.config.dev.js',
     ]);
   });
 
